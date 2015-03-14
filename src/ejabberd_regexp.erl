18df8493b27572f184c18982bd6a08cdc12132ed
Artem Sheremet <dot.doom@gmail.com>
2015-03-14 11:14:54 +0100

    Move impl of regexp:sh_to_awk into ejabberd
    For update to R15B, where regexp mod is deprecated and obsolete.

--- src/ejabberd_regexp.erl
+++ src/ejabberd_regexp.erl
@@ -66,7 +66,59 @@ greplace(String, Regexp, New) ->
 	A -> A
     end.
 
-sh_to_awk(ShRegExp) ->
-    case exec(xmerl_regexp, sh_to_awk, [ShRegExp], regexp, sh_to_awk, [ShRegExp]) of
-	A -> A
+%%sh_to_awk(ShRegExp) ->
+%%    case exec(xmerl_regexp, sh_to_awk, [ShRegExp], regexp, sh_to_awk, [ShRegExp]) of
+%%	A -> A
+%%    end.
+
+sh_to_awk(Pattern) when is_list(Pattern) ->
+    fnmatch_char(Pattern, [$(,$^]).
+
+fnmatch_char([], Acc) ->
+    lists:reverse([$$,$)|Acc]);
+fnmatch_char([Ch|Tail], Acc) ->
+    case Ch of
+      $\\ ->
+          fnmatch_char(Tail, [$\\,$\\|Acc]);
+      $[ ->
+          fnmatch_bexp_first(Tail, [Ch|Acc]);
+      $? ->
+          fnmatch_char(Tail, [$.|Acc]);
+      $* ->
+          fnmatch_char(Tail, [$*,$.|Acc]);
+      _ ->
+          case lists:member(Ch, "^.+{}()$|\\") of
+              true ->
+                  fnmatch_char(Tail, [Ch,$\\|Acc]);
+              false ->
+                  fnmatch_char(Tail, [Ch|Acc])
+          end
+    end.
+
+fnmatch_bexp_first([], _) ->
+    {error, unclosed_be};
+fnmatch_bexp_first([Ch|Tail], Acc) ->
+    case Ch of
+      $] ->
+          {error, empty_be};
+      $! ->
+          fnmatch_bexp_next(Tail, false, [$^|Acc]);
+      $\\ ->
+          fnmatch_bexp_next(Tail, true, Acc);
+      _ ->
+          fnmatch_bexp_next(Tail, false, [Ch|Acc])
+    end.
+
+fnmatch_bexp_next([], _, _) ->
+    {error, unclosed_be};
+fnmatch_bexp_next([Ch|Tail], true, Acc) ->
+    fnmatch_bexp_next(Tail, false, [Ch,$\\|Acc]);
+fnmatch_bexp_next([Ch|Tail], false, Acc) ->
+    case Ch of
+      $] ->
+          fnmatch_char(Tail, [Ch|Acc]);
+      $\\ ->
+          fnmatch_bexp_next(Tail, true, Acc);
+      _ ->
+          fnmatch_bexp_next(Tail, false, [Ch|Acc])
     end.
